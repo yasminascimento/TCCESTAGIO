0ee2d2bdd7ba998348fc54f10dcd82c5d52645af
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2024-08-01 06:23:20 -0300

    Create entrarcomo.php
    página já responsiva
    autor: yasmin
    css:ok

---
+++ entrarcomo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sistema de Estágio</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+/*página de entrar como já responsiva */ 
+                /*página de entrar como já responsiva */
+                                      /*página de entrar como já responsiva */
+                                                                       /*página de entrar como já responsiva */
+
+<body  class="entrarcomo">
+    <div class="conteudo">
+        <div class="texto">
+            <h1 style="margin-bottom: 20px;">Olá, Seja Bem-vindo</h1>
+            <h2 style="margin-bottom: 10px;">Entrar como:</h2>
+            <div class="btn-container">
+                <button type="button" id="responsavelBtn" class="btnentrar">Responsável</button>
+                <button type="button" id="diretorBtn" class="btnentrar">Diretor</button>
+            </div>
+        </div>
+        <div class="imagem">
+            <img src="https://setting.com.br/wp-content/uploads/2018/04/ferramentas-gestao-de-projetos.jpg" alt="Descrição da imagem">
+        </div>
+    </div>
+</body>
