ca9d0a66af1102a4650245595400cbd4741e05b2
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2024-08-08 07:12:21 -0300

    Update calendario.php

--- calendario.php
+++ calendario.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calendário</title>
-    <link rel="stylesheet" href="../views/style.css">
-    <link href='https://fullcalendar.io/releases/fullcalendar/5.9.0/main.min.css' rel='stylesheet' />
-    <script src='https://fullcalendar.io/releases/fullcalendar/5.9.0/main.min.js'></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
-</head>
-
-<body>
-
 
     <div class="container">
         <div class="left">
